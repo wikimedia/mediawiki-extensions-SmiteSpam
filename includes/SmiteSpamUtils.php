cca510c75b8cbfd02f7d72b249bf43614559a022
Yaron Koren <yaron57@gmail.com>
2025-04-22 22:10:50 +0000

    Further removal of pre-MW 1.40 compatibility
    Change-Id: Ia6cdfd16fef9e41479cf51285f3ec29e041a5fc6

--- includes/SmiteSpamUtils.php
+++ includes/SmiteSpamUtils.php
@@ -7,31 +7,21 @@ class SmiteSpamUtils {
 	/**
 	 * Provides a database for read access.
 	 *
-	 * @return IDatabase
+	 * @return \Wikimedia\Rdbms\IReadableDatabase
 	 */
 	public static function getReadDB() {
-		$lbFactory = MediaWikiServices::getInstance()->getDBLoadBalancerFactory();
-		if ( method_exists( $lbFactory, 'getReplicaDatabase' ) ) {
-			// MW 1.40+
-			return $lbFactory->getReplicaDatabase();
-		} else {
-			return $lbFactory->getMainLB()->getConnection( DB_REPLICA );
-		}
+		return MediaWikiServices::getInstance()->getDBLoadBalancerFactory()
+			->getReplicaDatabase();
 	}
 
 	/**
 	 * Provides a database for write access.
 	 *
-	 * @return IDatabase
+	 * @return \Wikimedia\Rdbms\IDatabase
 	 */
 	public static function getWriteDB() {
-		$lbFactory = MediaWikiServices::getInstance()->getDBLoadBalancerFactory();
-		if ( method_exists( $lbFactory, 'getPrimaryDatabase' ) ) {
-			// MW 1.40+
-			return $lbFactory->getPrimaryDatabase();
-		} else {
-			return $lbFactory->getMainLB()->getConnection( DB_PRIMARY );
-		}
+		return MediaWikiServices::getInstance()->getDBLoadBalancerFactory()
+			->getPrimaryDatabase();
 	}
 
 }
